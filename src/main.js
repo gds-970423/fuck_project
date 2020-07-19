2c54816834b2d8612391ff29e63710a4a41a8ba8
laogao <871593435@qq.com>
2020-07-19 10:31:27 +0800

    把主线分支合并到login分支上

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router/index.js'
 import './plugins/element.js'
 // 引入全局样式表
 import './assets/css/global.css'
-// 导入 字体图标
+//  导入字体图标
 import './assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
